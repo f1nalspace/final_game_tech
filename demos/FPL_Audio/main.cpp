ddeefcd299f85a6a4229a102ff013770d74646c9
Finalspace <finalspace@googlemail.com>
2017-12-11 16:44:14 +0100

    Comments for audio demo

--- demos/FPL_Audio/main.cpp
+++ demos/FPL_Audio/main.cpp
@@ -1,9 +1,24 @@
+/*
+*******************************************************************************
+FPL Simple Audio Playback Demo
+*******************************************************************************
+This demo plays a contiguous sine or square wave in the expected S16 format.
+
+- You fill out the audio settings you want or leave the default
+- You fill out the audio client read callback and user data pointer
+- You initialize the platform with InitFlags::Audio and pass your settings as second parameter
+- You start playing the audio using PlayAudio()
+- When you are done you stop it using StopAudio()
+- You release the platform
+- Done
+*/
+
 #define FPL_IMPLEMENTATION
 #define FPL_AUTO_NAMESPACE
 #define FPL_NO_WINDOW
 #include <final_platform_layer.hpp>
-#include <math.h>
-#include <stdio.h>
+#include <math.h> // sinf
+#include <stdio.h> // getchar
 
 struct AudioTest {
 	uint32_t toneHz;
@@ -41,25 +56,39 @@ static uint32_t FillAudioBuffer(const AudioDeviceFormat &nativeFormat, const uin
 int main(int argc, char **args) {
 	int result = -1;
 
+	// Initialize to default settings which is 48 KHz and 2 Channels
 	Settings settings = DefaultSettings();
 
+	// Optionally overwrite audio settings if needed
+
+	// Setup some state for the sine/square wave generation
 	AudioTest audioTest = {};
 	audioTest.toneHz = 256;
 	audioTest.toneVolume = 1000;
 	audioTest.wavePeriod = settings.audio.desiredFormat.sampleRate / audioTest.toneHz;
 	audioTest.useSquareWave = false;
 
+	// Provide client read callback and optionally user data
 	settings.audio.clientReadCallback = FillAudioBuffer;
 	settings.audio.userData = &audioTest;
 
+	// Initialize the platform with audio enabled and the settings
 	if (InitPlatform(InitFlags::Audio, settings)) {
+		// You can overwrite the client read callback and user data if you want to
+		SetAudioClientReadCallback(FillAudioBuffer, &audioTest);
+		// Start audio playback (This will start calling clientReadCallback regulary)
 		if (PlayAudio() == AudioResult::Success) {
+			// Print out the native audio format
 			AudioDeviceFormat nativeFormat = GetAudioNativeFormat();
 			ConsoleFormatOut("Audio with %lu KHz and %lu channels is playing, press any key to stop playback...\n", nativeFormat.sampleRate, nativeFormat.channels);
+			// Wait for any key presses
 			getchar();
+			// Stop audio playback
 			StopAudio();
 		}
+		// Release the platform
 		ReleasePlatform();
+		result = 0;
 	}
 	return(result);
 }
