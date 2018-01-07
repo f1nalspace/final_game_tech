558f0c1128b4c63e070fe42c468f32073f739c8f
Finalspace <finalspace@googlemail.com>
2018-01-07 08:58:01 +0100

    Changed all the makefiles to use c++/11

--- demos/clang_x64_release.bat
+++ demos/clang_x64_release.bat
@@ -2,4 +2,4 @@ set BUILD_DIR=bin\FPL_Console\x64-Release
 set IGNORED_WARNINGS=-Wno-missing-field-initializers -Wno-sign-conversion -Wno-cast-qual -Wno-unused-parameter -Wno-format-nonliteral -Wno-old-style-cast -Wno-header-hygiene
 rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
-clang -Weverything %IGNORED_WARNINGS% -DFPL_RELEASE -std=c++98 -s -O2 -I..\ -lkernel32.lib -o%BUILD_DIR%\FPL_Console.exe FPL_Console\main.cpp
+clang -Weverything %IGNORED_WARNINGS% -DFPL_RELEASE -std=c++11 -s -O2 -I..\ -lkernel32.lib -o%BUILD_DIR%\FPL_Console.exe FPL_Console\main.cpp
--- demos/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p bin/linux_x64
-g++ FPL_Linux/main.cpp -std=c++11 -I../ -o bin/linux_x64/fpl_linux 2> logcompile.txt
-g++ FPL_Console/main.cpp -std=c++11 -I../ -o bin/linux_x64/fpl_console 2> logcompile.txt
-cat logcompile.txt
