b48d4b06f3f3a7355015e4f4880e4be42f1b8d38
Finalspace <finalspace@googlemail.com>
2018-01-07 09:04:57 +0100

    Updated clang build files

--- demos/clang_x64_debug.bat
+++ demos/clang_x64_debug.bat
@@ -1,5 +1,5 @@
 set BUILD_DIR=bin\FPL_Console\x64-Debug
-set IGNORED_WARNINGS=-Wno-missing-field-initializers -Wno-sign-conversion -Wno-cast-qual -Wno-unused-parameter -Wno-format-nonliteral -Wno-old-style-cast -Wno-header-hygiene
+set IGNORED_WARNINGS=-Wno-missing-field-initializers -Wno-sign-conversion -Wno-cast-qual -Wno-unused-parameter -Wno-format-nonliteral -Wno-old-style-cast -Wno-header-hygiene -Wno-c++98-compat -Wno-c++98-compat-pedantic
 rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
-clang -g -Weverything %IGNORED_WARNINGS% -DFPL_DEBUG -std=c++11 -O0 -I..\ -lkernel32.lib -o%BUILD_DIR%\FPL_Console.exe FPL_Console\main.cpp > error.txt 2>&1
+clang -std=c++11 -g -Weverything %IGNORED_WARNINGS% -DFPL_DEBUG -O0 -I..\ -lkernel32.lib -o%BUILD_DIR%\FPL_Console.exe FPL_Console\main.cpp > error.txt 2>&1
--- demos/clang_x64_release.bat
+++ demos/clang_x64_release.bat
@@ -1,5 +1,5 @@
 set BUILD_DIR=bin\FPL_Console\x64-Release
-set IGNORED_WARNINGS=-Wno-missing-field-initializers -Wno-sign-conversion -Wno-cast-qual -Wno-unused-parameter -Wno-format-nonliteral -Wno-old-style-cast -Wno-header-hygiene
+set IGNORED_WARNINGS=-Wno-missing-field-initializers -Wno-sign-conversion -Wno-cast-qual -Wno-unused-parameter -Wno-format-nonliteral -Wno-old-style-cast -Wno-header-hygiene -Wno-c++98-compat -Wno-c++98-compat-pedantic
 rmdir /s /q %BUILD_DIR%
 mkdir %BUILD_DIR%
-clang -Weverything %IGNORED_WARNINGS% -DFPL_RELEASE -std=c++11 -s -O2 -I..\ -lkernel32.lib -o%BUILD_DIR%\FPL_Console.exe FPL_Console\main.cpp
+clang -std=c++11 -Weverything %IGNORED_WARNINGS% -DFPL_RELEASE -s -O2 -I..\ -lkernel32.lib -o%BUILD_DIR%\FPL_Console.exe FPL_Console\main.cpp
