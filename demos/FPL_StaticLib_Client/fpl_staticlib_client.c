d14736395cccda93620fffda6ed3b2b10da3120c
Finalspace <finalspace@googlemail.com>
2018-04-25 17:07:02 +0100

    Added fpl demo "StaticLib"

---
+++ demos/FPL_StaticLib_Client/fpl_staticlib_client.c
@@ -0,0 +1,29 @@
+/*
+-------------------------------------------------------------------------------
+Name:
+	FPL-Demo | StaticLib_Client
+Description:
+	Client application using the FPL static library
+Requirements:
+	StaticLib_Host (Static-Library)
+Author:
+	Torsten Spaete
+Changelog:
+	## 2018-04-24:
+	- Initial creation of this description block
+-------------------------------------------------------------------------------
+*/
+
+#define FPL_ENTRYPOINT // Force the inclusion of the entry point
+#include <final_platform_layer.h>
+
+int main(int argc, char **argv) {
+	if(fplPlatformInit(fplInitFlags_All, fpl_null)) {
+		fplEvent ev;
+		while(fplWindowUpdate(&ev)) {
+			fplClearEvents();
+		}
+		fplPlatformRelease();
+	}
+	return 0;
+}
---
+++ demos/FPL_StaticLib_Host/final_platform_layer.c
@@ -0,0 +1,19 @@
+/*
+-------------------------------------------------------------------------------
+Name:
+	FPL-Demo | StaticLib_Host
+Description:
+	FPL implementation compiled in a static library
+Requirements:
+	No requirements
+Author:
+	Torsten Spaete
+Changelog:
+	## 2018-04-24:
+	- Initial creation of this description block
+-------------------------------------------------------------------------------
+*/
+
+#define FPL_IMPLEMENTATION // Include the implementation
+#define FPL_NO_ENTRYPOINT // Disable any point entry inclusion
+#include <final_platform_layer.h>
