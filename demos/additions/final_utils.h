6a1b85ccc708d3064a156ccf4a6c4b5d618f1dbb
Finalspace <finalspace@googlemail.com>
2018-07-04 18:25:03 +0100

    Added final_utils.h

---
+++ demos/additions/final_utils.h
@@ -0,0 +1,15 @@
+#ifndef FINAL_UTILS_H
+#define FINAL_UTILS_H
+
+template <typename T>
+inline T PointerToValue(void *ptr) {
+	T result = (T)(uintptr_t)(ptr);
+	return(result);
+}
+template <typename T>
+inline void *ValueToPointer(T value) {
+	void *result = (T *)(uintptr_t)(value);
+	return(result);
+}
+
+#endif // FINAL_UTILS_H
