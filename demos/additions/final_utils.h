c546b391bd95d6d789e5b0df91263da85918ad3d
Finalspace <finalspace@googlemail.com>
2018-07-06 19:34:34 +0100

    Added utility header file

--- demos/additions/final_utils.h
+++ demos/additions/final_utils.h
@@ -1,5 +1,21 @@
+/*
+Name:
+	Final Utils
+
+Description:
+	Contains utility functions.
+
+	This file is part of the final_framework.
+
+License:
+	MIT License
+	Copyright 2018 Torsten Spaete
+*/
+
 #ifndef FINAL_UTILS_H
 #define FINAL_UTILS_H
+
+#include <final_platform_layer.h>
 
 template <typename T>
 inline T PointerToValue(void *ptr) {
@@ -11,5 +27,40 @@ inline void *ValueToPointer(T value) {
 	void *result = (T *)(uintptr_t)(value);
 	return(result);
 }
+
+template <typename T>
+inline void Swap(T &a, T &b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T, size_t N>
+inline size_t ArrayCount(T(&arr)[N]) {
+	size_t result = sizeof(arr) / sizeof(arr[0]);
+	return(result);
+}
+
+// @BAD(final): CPP is such garbage!
+// It cannot handle array index initializer such as [index] = value :-(
+// So we need this nonsense just to initialize a static array -.-template <typename TIndexType, typename TValueType, size_t size>
+template <typename TIndexType, typename TValueType, size_t valueCount>
+class ArrayInitializer {
+protected:
+	TValueType a[valueCount];
+public:
+	ArrayInitializer() {
+		fplMemoryClear(a, sizeof(TValueType) * ArrayCount(a));
+	}
+	const TValueType &operator [] (TIndexType eindex) const {
+		return a[(int)eindex];
+	}
+	TValueType &operator [] (TIndexType eindex) {
+		return a[(int)eindex];
+	}
+	void Set(TIndexType e, const TValueType &value) {
+		a[(int)e] = value;
+	}
+};
 
 #endif // FINAL_UTILS_H
