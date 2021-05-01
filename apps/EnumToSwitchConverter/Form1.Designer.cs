17fd17c36697bdd9de99af9581646dd94a3648a2
f1nalspace <finalspace@googlemail.com>
2021-05-01 11:36:09 +0200

    Added enum to switch converter

---
+++ apps/EnumToSwitchConverter/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace EnumToSwitchConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.tbDest = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbSource
+            // 
+            this.tbSource.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSource.Location = new System.Drawing.Point(12, 12);
+            this.tbSource.Multiline = true;
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(321, 426);
+            this.tbSource.TabIndex = 0;
+            this.tbSource.Text = resources.GetString("tbSource.Text");
+            this.tbSource.WordWrap = false;
+            // 
+            // tbDest
+            // 
+            this.tbDest.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDest.Location = new System.Drawing.Point(467, 12);
+            this.tbDest.Multiline = true;
+            this.tbDest.Name = "tbDest";
+            this.tbDest.ReadOnly = true;
+            this.tbDest.Size = new System.Drawing.Size(321, 426);
+            this.tbDest.TabIndex = 1;
+            this.tbDest.WordWrap = false;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(362, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "button1";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.tbDest);
+            this.Controls.Add(this.tbSource);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.TextBox tbDest;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
