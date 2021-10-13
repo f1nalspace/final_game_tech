b75d158fb2b82d6eec4ad2cb6840ba0419f03dd5
f1nalspace <finalspace@googlemail.com>
2021-10-13 12:32:03 +0200

    Removed WS_VISIBLE flag to prevent window resizing when starting in fullscreen

--- final_platform_layer.h
+++ final_platform_layer.h
@@ -10736,7 +10736,7 @@ fpl_common_api void fplDebugFormatOut(const char *format, ...) {
 #if defined(FPL__ENABLE_WINDOW)
 
 fpl_internal DWORD fpl__Win32MakeWindowStyle(const fplWindowSettings *settings) {
-	DWORD result = WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_VISIBLE;
+	DWORD result = WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
 	if(settings->isFullscreen || !settings->isDecorated) {
 		result |= WS_POPUP;
 	} else {
