9f2fb4e126b54b2f731ec76bb5cce5199fc563da
Finalspace <finalspace@gmail.com>
2025-07-29 18:26:03 +0200

    [Demo/FPL_Input] Added premake vpaths for dependencies

--- demos/FPL_Input/premake5.lua
+++ demos/FPL_Input/premake5.lua
@@ -4,6 +4,41 @@ project "FPL_Input"
 	language "C++"
 	cppdialect "C++11"
 	
-	files { "fpl_input.cpp" }
+	files { 
+		"fpl_input.cpp", 
+		"../../final_platform_layer.h",
+		"../../final_dynamic_opengl.h",
+		"../dependencies/stb/stb_image.h",
+		"../dependencies/stb/stb_truetype.h",
+		"../additions/final_fonts.h",
+	}
 	
-	postbuildcommands { "{COPYDIR} %[%{!prj.location}/data] %[%{!cfg.targetdir}/data/]" }		
+	filter { "files:../../final_platform_layer.h", "files:../../final_dynamic_opengl.h" }
+		flags { "ExcludeFromBuild" }
+	filter {}
+
+	filter { "files:../dependencies/stb/stb_image.h", "files:../dependencies/stb/stb_truetype.h" }
+		flags { "ExcludeFromBuild" }
+	filter {}
+
+	filter { "files:../additions/final_fonts.h" }
+		flags { "ExcludeFromBuild" }
+	filter {}
+
+	group "Dependencies"
+		vpaths { 
+			["Dependencies"] = {
+				"../../final_platform_layer.h",
+				"../../final_dynamic_opengl.h",
+			},
+			["ThirdParty"] = {
+				"../dependencies/stb/stb_image.h",
+				"../dependencies/stb/stb_truetype.h",
+			},
+			["Additions"] = {
+				"../additions/final_fonts.h",
+			},
+		}
+	group "" -- end of "Dependencies"
+		
+	postbuildcommands { "{COPYDIR} %[%{!prj.location}/data] %[%{!cfg.targetdir}/data/]" }
