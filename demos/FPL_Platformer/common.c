0c221354852968cb97fdd4ccf8bac839d1ae662c
Finalspace <finalspace@gmail.com>
2025-11-23 09:47:15 +0100

    Common utils for Guid/Hex/JSON parsing

---
+++ demos/FPL_Platformer/common.c
@@ -0,0 +1,97 @@
+#include "common.h"
+
+extern uint32_t ParseHexU32(const char *str, const size_t len) {
+	if (str == fpl_null || len == 0) {
+		return 0; // Invalid arguments
+	}
+	const char *p = str;
+	uint32_t value = 0;
+	while (*p && ((size_t)(p - str) < len)) {
+		char c = *p;
+		int v;
+		if (c >= '0' && c <= '9') {
+			v = (int)(*p - '0');
+		} else if (c >= 'a' && c <= 'f') {
+			v = (int)(*p - 'a') + 10;
+		} else if (c >= 'A' && c <= 'F') {
+			v = (int)(*p - 'A') + 10;
+		} else {
+			return 0;
+		}
+		value *= 16;
+		value += (uint32_t)v;
+		++p;
+	}
+	uint32_t result = (uint32_t)value;
+	return(result);
+}
+
+extern bool TryIID16ParseString(const char *str, const size_t len, IID16 *outValue) {
+	if (str == fpl_null || len == 0 || outValue == fpl_null) {
+		return false; // Invalid arguments
+	}
+	if (len != IID16_STRING_LENGTH) {
+		return false; // String too short or too long
+	}
+	fplClearStruct(outValue);
+	int pos = 0;
+	outValue->a = ParseHexU32(str + pos, 8);
+	pos += (8 + 1);
+	outValue->b = (uint16_t)ParseHexU32(str + pos, 4);
+	pos += (4 + 1);
+	outValue->c = (uint16_t)ParseHexU32(str + pos, 4);
+	pos += (4 + 1);
+
+	int minusoffset = 0;
+	for (int x = 0; x < 16; x += 2) {
+		if (x == 4) {
+			minusoffset = 1; // We don't want the "minus" to be included in the the byte
+		}
+		int byteindex = x / 2;
+		const char *part = str + (pos + x + minusoffset);
+		outValue->d[byteindex] = (uint8_t)ParseHexU32(part, 2);
+	}
+
+	return true;
+}
+
+extern IID16 IID16ParseString(const char *str, const size_t len) {
+	IID16 result;
+	if (TryIID16ParseString(str, len, &result)) {
+		return result;
+	}
+	return IID16Empty;
+}
+
+extern IID16 IID16ParseStringType(const String str) {
+	return IID16ParseString(str.str, str.len);
+}
+
+extern json_object_element_t *JSONObjectFindElementByName(json_object_t *obj, const char *name) {
+	size_t nameLen = fplGetStringLength(name);
+	if (obj == fpl_null || nameLen == 0) {
+		return fpl_null;
+	}
+	json_object_element_t *cur = obj->start;
+	while (cur != fpl_null) {
+		if (cur->name != fpl_null && fplIsStringEqualLen(name, nameLen, cur->name->string, cur->name->string_size)) {
+			return cur;
+		}
+		cur = cur->next;
+	}
+	return fpl_null;
+}
+
+extern json_value_t *JSONObjectFindValueByName(json_object_t *obj, const char *name) {
+	size_t nameLen = fplGetStringLength(name);
+	if (obj == fpl_null || nameLen == 0) {
+		return fpl_null;
+	}
+	json_object_element_t *element = JSONObjectFindElementByName(obj, name);
+	if (element == fpl_null) {
+		return fpl_null;
+	}
+	return element->value;
+}
+
+
---
+++ demos/FPL_Platformer/common.h
@@ -0,0 +1,99 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <final_platform_layer.h>
+
+#include "json/json.h"
+
+typedef struct String {
+	const char *str;
+	size_t len;
+} String;
+
+fpl_inline String StringInit(const char *str, const size_t len) {
+	String result = fplStructInit(String, str, len);
+	return result;
+}
+
+#define StringEmpty fplStructInit(String, 0)
+
+#define IID16_STRING_LENGTH 36
+
+typedef struct IID16 {
+	uint32_t a;
+	uint16_t b;
+	uint16_t c;
+	uint8_t d[8];
+} IID16;
+
+#define IID16Empty fplStructInit(IID16, 0)
+
+fpl_inline IID16 IID16Init(const uint32_t a, const uint16_t b, const uint16_t c, const uint8_t d[8]) {
+	IID16 result = fplZeroInit;
+	result.a = a;
+	result.b = b;
+	result.c = c;
+	for (uint8_t i = 0; i < 8; ++i)
+		result.d[i] = d[i];
+	return result;
+}
+
+// Tries to parse a IID/GUID string such as "5bb13300-ac70-11f0-842c-7f4fc27d982e" into a IID16 structure
+extern bool TryIID16ParseString(const char *str, const size_t len, IID16 *outValue);
+
+// Parse a IID/GUID string such as "5bb13300-ac70-11f0-842c-7f4fc27d982e" into a IID16 structure
+extern IID16 IID16ParseString(const char *str, const size_t len);
+extern IID16 IID16ParseStringType(const String str);
+
+extern json_object_element_t *JSONObjectFindElementByName(json_object_t *obj, const char *name);
+extern json_value_t *JSONObjectFindValueByName(json_object_t *obj, const char *name);
+
+fpl_inline json_object_t *JSONValueAsObject(json_value_t *value) {
+	if (value == fpl_null || value->type != json_type_object || value->payload == fpl_null) {
+		return fpl_null;
+	}
+	json_object_t *result = (json_object_t *)value->payload;
+	return result;
+}
+
+fpl_inline json_array_t *JSONValueAsArray(json_value_t *value) {
+	if (value == fpl_null || value->type != json_type_array || value->payload == fpl_null) {
+		return fpl_null;
+	}
+	json_array_t *result = (json_array_t *)value->payload;
+	return result;
+}
+
+fpl_inline String JSONValueAsString(json_value_t *value) {
+	if (value == fpl_null || value->type != json_type_string || value->payload == fpl_null) {
+		return StringEmpty;
+	}
+	json_string_t *str = (json_string_t *)value->payload;
+	if (str->string_size == 0) {
+		return StringEmpty;
+	}
+	String result = StringInit(str->string, str->string_size);
+	return result;
+}
+
+fpl_inline String JSONElementAsString(json_object_element_t *element) {
+	if (element == fpl_null || element->value == fpl_null) {
+		return StringEmpty;
+	}
+	json_value_t *value = element->value;
+	return JSONValueAsString(value);
+}
+
+fpl_inline int32_t JSONValueAsS32(json_value_t *value, const uint32_t defaultValue) {
+	if (value == fpl_null || value->type != json_type_number || value->payload == fpl_null) {
+		return defaultValue;
+	}
+	json_number_t *number = (json_number_t *)value->payload;
+	if (number->number == fpl_null || number->number_size == 0) {
+		return defaultValue;
+	}
+	int32_t result = fplStringToS32Len(number->number, number->number_size);
+	return result;
+}
+
+#endif // COMMON_H
