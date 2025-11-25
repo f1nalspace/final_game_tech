343369078677ce815b9278762ddda4581eeaac3f
Finalspace <finalspace@gmail.com>
2025-11-25 16:28:08 +0100

    Define tileset a positions

--- demos/FPL_Platformer/assets.h
+++ demos/FPL_Platformer/assets.h
@@ -13,6 +13,52 @@
 // Size of the assets persistent memory block
 #define GAME_ASSETS_MEMORY_PERSISTENT_SIZE fplMegaBytes(4)
 
+// Tileset A Border 3x3
+#define TILESET_A_BORDER_3x3_OFFSET V2iInit(7, 0)
+#define TILESET_A_BORDER_3x3_TOP_LEFT V2iInit(0, 0)
+#define TILESET_A_BORDER_3x3_TOP_MIDDLE V2iInit(1, 0)
+#define TILESET_A_BORDER_3x3_TOP_RIGHT V2iInit(2, 0)
+#define TILESET_A_BORDER_3x3_SIDE_LEFT V2iInit(0, 1)
+#define TILESET_A_BORDER_3x3_MIDDLE V2iInit(1, 1)
+#define TILESET_A_BORDER_3x3_SIDE_RIGHT V2iInit(2, 1)
+#define TILESET_A_BORDER_3x3_BOTTOM_LEFT V2iInit(0, 2)
+#define TILESET_A_BORDER_3x3_BOTTOM_MIDDLE V2iInit(1, 2)
+#define TILESET_A_BORDER_3x3_BOTTOM_RIGHT V2iInit(2, 2)
+
+// Tileset A Collidable 3x3
+#define TILESET_A_COLLIDABLE_3x3_OFFSET V2iInit(0, 0)
+#define TILESET_A_COLLIDABLE_3x3_TOP_LEFT V2iInit(0, 0)
+#define TILESET_A_COLLIDABLE_3x3_TOP_MIDDLE V2iInit(1, 0)
+#define TILESET_A_COLLIDABLE_3x3_TOP_RIGHT V2iInit(2, 0)
+#define TILESET_A_COLLIDABLE_3x3_SIDE_LEFT V2iInit(0, 1)
+#define TILESET_A_COLLIDABLE_3x3_MIDDLE V2iInit(1, 1)
+#define TILESET_A_COLLIDABLE_3x3_SIDE_RIGHT V2iInit(2, 1)
+#define TILESET_A_COLLIDABLE_3x3_BOTTOM_LEFT V2iInit(0, 2)
+#define TILESET_A_COLLIDABLE_3x3_BOTTOM_MIDDLE V2iInit(1, 2)
+#define TILESET_A_COLLIDABLE_3x3_BOTTOM_RIGHT V2iInit(2, 2)
+
+// Tileset A Collidable 1x3
+#define TILESET_A_COLLIDABLE_1x3_OFFSET V2iInit(3, 0)
+#define TILESET_A_COLLIDABLE_1x3_TOP V2iInit(0, 0)
+#define TILESET_A_COLLIDABLE_1x3_MIDDLE V2iInit(0, 1)
+#define TILESET_A_COLLIDABLE_1x3_BOTTOM V2iInit(0, 2)
+
+// Tileset A Collidable 3x1
+#define TILESET_A_COLLIDABLE_3x1_OFFSET V2iInit(4, 0)
+#define TILESET_A_COLLIDABLE_3x1_LEFT V2iInit(0, 0)
+#define TILESET_A_COLLIDABLE_3x1_CENTER V2iInit(1, 0)
+#define TILESET_A_COLLIDABLE_3x1_RIGHT V2iInit(2, 0)
+
+// Tileset A Collidable 1x1
+#define TILESET_A_COLLIDABLE_1x1_A V2iInit(4, 1)
+#define TILESET_A_COLLIDABLE_1x1_B V2iInit(5, 1)
+
+// Tileset A Collidable 3x0.5
+#define TILESET_A_COLLIDABLE_3x0_5_OFFSET V2iInit(4, 2)
+#define TILESET_A_COLLIDABLE_3x0_5_LEFT V2iInit(0, 0)
+#define TILESET_A_COLLIDABLE_3x0_5_CENTER V2iInit(1, 0)
+#define TILESET_A_COLLIDABLE_3x0_5_RIGHT V2iInit(2, 0)
+
 typedef struct GameAssets {
 	fmemMemoryBlock transientMemory;
 	fmemMemoryBlock persistentMemory;
