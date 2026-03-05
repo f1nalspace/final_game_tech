0713d437a9b16b0ff731e20305ec17f586b3cbcf
Finalspace <finalspace@gmail.com>
2026-03-05 21:51:15 +0100

    Fixed small changelog typo for #181

--- final_platform_layer.h
+++ final_platform_layer.h
@@ -199,7 +199,7 @@ SOFTWARE.
 
 	- Fixed: [X11] Fixed last event from event queue was never used, when there is no events from the window
 	- Fixed: [X11] Fixed window support was not disabled when X11 is not present
-	- Fixed[181]: fpl__X11ParseUriPaths does not do any URI decoding, resulting in most-likely unuseable file paths
+	- Fixed[181]: [X11] fpl__X11ParseUriPaths does not do any URI decoding, resulting in most-likely unuseable file paths
 
 	- Fixed: [POSIX] Fixed pthread fpl__POSIXSemaphoreHandle was not used
 	- Fixed: [POSIX] dirint.h and sched.h was not included always
