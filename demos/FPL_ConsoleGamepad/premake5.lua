c3f2a7b5ecd5ef7224cfea27efbe83eb93607594
Finalspace <finalspace@gmail.com>
2026-05-11 11:29:51 +0200

    Renamed FPL_ConsoleGamepad project and makefiles

--- demos/FPL_ConsoleGamepad/premake5.lua
+++ demos/FPL_ConsoleGamepad/premake5.lua
@@ -1,7 +1,7 @@
-project "FPL_ConsoleGamepad"
+project "FPL_ConsoleInput"
    kind "ConsoleApp"
 
    language "C"
    cdialect "C99"
 
-   files { "fpl_console_gamepad.c" }
+   files { "fpl_console_input.c" }
