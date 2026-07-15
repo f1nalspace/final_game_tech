fd3850f0c29ab8930113e83d0f2b311e6eea4f96
Finalspace <finalspace@gmail.com>
2026-07-15 17:19:41 +0200

    - Added mutex to Log to make it thread-safe and removed global storages

--- demos/additions/final_log.h
+++ demos/additions/final_log.h
@@ -7,6 +7,10 @@ Description:
 
 	This file is part of the final_framework.
 
+Changelog:
+	## 2026-07-15
+	- Added mutex to Log to make it thread-safe and removed global storages
+
 License:
 	MIT License
 	Copyright 2017-2026 Torsten Spaete
@@ -45,12 +49,12 @@ typedef struct LogString {
 typedef struct Log {
 	// Memory block for string buffer
 	fmemMemoryBlock memory;
-	// Temporary character buffer, that maps to the memory block
-	LogString charBuffer;
 	// Handle to the active log file
 	fplFileHandle fileHandle;
+	// Synchronization primitive for thread safety
+	fplMutexHandle mutex;
 	// Indicates whether the log system has been initialized
-	bool isInitialized;
+	volatile bool isInitialized;
 } Log;
 
 fpl_extern bool LogInit(const char *logFilePath);
@@ -79,19 +83,14 @@ fpl_internal const char *gLogLevelNames[] = {
 	"  TRACE",
 };
 
-
-static Log gLog = { 0 };
-
-static char gLogWriteData[256] = { 0 };
-static LogString gLogWriteString = {
-	gLogWriteData,
-	0,
-	sizeof(gLogWriteData),
-	0,
-};
+static Log gLog = fplZeroInit;
 
 fpl_extern bool LogInit(const char *logFilePath) {
-	if (fplGetStringLength(logFilePath) == 0) {
+	if (logFilePath == fpl_null || fplGetStringLength(logFilePath) == 0) {
+		return false;
+	}
+
+	if (!fplIsPlatformInitialized()) {
 		return false;
 	}
 
@@ -101,28 +100,53 @@ fpl_extern bool LogInit(const char *logFilePath) {
 
 	fplClearStruct(&gLog);
 
+	if (!fplMutexInit(&gLog.mutex)) {
+		return false;
+	}
+
 	fmemMemoryBlock *mem = &gLog.memory;
 
 	if (!fmemInit(mem, fmemType_Growable, fplMegaBytes(16), 0)) {
+		fplMutexDestroy(&gLog.mutex);
 		return false;
 	}
 
 	bool success = fplFileAppendBinary(logFilePath, &gLog.fileHandle);
+	if (!success) {
+		fmemFree(mem);
+		fplMutexDestroy(&gLog.mutex);
+		return false;
+	}
 
-	gLog.isInitialized = success;
+	gLog.isInitialized = true;
 
-	return success;
+	return true;
 }
 
 fpl_extern void LogShutdown() {
 	if (!gLog.isInitialized) {
 		return;
 	}
+
+	fplMutexLock(&gLog.mutex);
+
 	if (gLog.fileHandle.isValid) {
 		fplFileClose(&gLog.fileHandle);
 	}
 	fmemFree(&gLog.memory);
+
+	fplMutexUnlock(&gLog.mutex);
+	fplMutexDestroy(&gLog.mutex);
+
 	fplClearStruct(&gLog);
+}
+
+static void LogWriteNewlineUnsafe(void) {
+	char buf[2];
+	buf[0] = '\n';
+	buf[1] = '\0';
+	fplFileWriteBlock(&gLog.fileHandle, buf, 1);
+	fplFileFlush(&gLog.fileHandle);
 }
 
 fpl_extern void LogWriteRaw(const char *format, ...) {
@@ -130,18 +154,19 @@ fpl_extern void LogWriteRaw(const char *format, ...) {
 		return;
 	}
 
-	if (format == NULL || fplGetStringLength(format) == 0) {
-		fplAssert(gLogWriteString.allocated >= 2);
-		gLogWriteString.length = 1;
-		gLogWriteString.data[0] = '\n';
-		gLogWriteString.data[1] = '\0';
-		fplFileWriteBlock(&gLog.fileHandle, gLogWriteString.data, gLogWriteString.length);
-		fplFileFlush(&gLog.fileHandle);
+	fplMutexLock(&gLog.mutex);
+
+	if (format == fpl_null || fplGetStringLength(format) == 0) {
+		LogWriteNewlineUnsafe();
+		fplMutexUnlock(&gLog.mutex);
 		return;
 	}
 
 	fmemMemoryBlock temporaryMemory = fplZeroInit;
-	fmemBeginTemporary(&gLog.memory, &temporaryMemory);
+	if (!fmemBeginTemporary(&gLog.memory, &temporaryMemory)) {
+		fplMutexUnlock(&gLog.mutex);
+		return;
+	}
 
 	LogString tempString = fplZeroInit;
 	tempString.allocated = temporaryMemory.size;
@@ -161,6 +186,8 @@ fpl_extern void LogWriteRaw(const char *format, ...) {
 	fplFileFlush(&gLog.fileHandle);
 
 	fmemEndTemporary(&temporaryMemory);
+
+	fplMutexUnlock(&gLog.mutex);
 }
 
 fpl_extern void LogWriteLineBreak() {
@@ -172,18 +199,19 @@ fpl_extern void LogWriteArgs(const LogLevel level, const char *format, va_list a
 		return;
 	}
 
+	fplMutexLock(&gLog.mutex);
+
 	if (fplGetStringLength(format) == 0) {
-		fplAssert(gLogWriteString.allocated >= 2);
-		gLogWriteString.length = 1;
-		gLogWriteString.data[0] = '\n';
-		gLogWriteString.data[1] = '\0';
-		fplFileWriteBlock(&gLog.fileHandle, gLogWriteString.data, gLogWriteString.length);
-		fplFileFlush(&gLog.fileHandle);
+		LogWriteNewlineUnsafe();
+		fplMutexUnlock(&gLog.mutex);
 		return;
 	}
 
 	fmemMemoryBlock temporaryMemory = fplZeroInit;
-	fmemBeginTemporary(&gLog.memory, &temporaryMemory);
+	if (!fmemBeginTemporary(&gLog.memory, &temporaryMemory)) {
+		fplMutexUnlock(&gLog.mutex);
+		return;
+	}
 
 	LogString tempString = fplZeroInit;
 	tempString.allocated = temporaryMemory.size;
@@ -222,6 +250,8 @@ fpl_extern void LogWriteArgs(const LogLevel level, const char *format, va_list a
 	fplFileFlush(&gLog.fileHandle);
 
 	fmemEndTemporary(&temporaryMemory);
+
+	fplMutexUnlock(&gLog.mutex);
 }
 
 fpl_extern void LogWrite(const LogLevel level, const char *format, ...) {
@@ -229,56 +259,10 @@ fpl_extern void LogWrite(const LogLevel level, const char *format, ...) {
 		return;
 	}
 
-	if (fplGetStringLength(format) == 0) {
-		fplAssert(gLogWriteString.allocated >= 2);
-		gLogWriteString.length = 1;
-		gLogWriteString.data[0] = '\n';
-		gLogWriteString.data[1] = '\0';
-		fplFileWriteBlock(&gLog.fileHandle, gLogWriteString.data, gLogWriteString.length);
-		fplFileFlush(&gLog.fileHandle);
-		return;
-	}
-
-	fmemMemoryBlock temporaryMemory = fplZeroInit;
-	fmemBeginTemporary(&gLog.memory, &temporaryMemory);
-
-	LogString tempString = fplZeroInit;
-	tempString.allocated = temporaryMemory.size;
-	tempString.length = 0;
-	tempString.data = (char *)temporaryMemory.base;
-
-	const char *levelName = gLogLevelNames[level];
-	fplDateTime utcDate = fplDateTimeQuery(fplDateTimeType_UTC);
-	fplDateTimeResult utcDateRes = fplFormatDateTime(utcDate, fplDateTimeType_UTC);
-
-	size_t len;
-	char *ptr = tempString.data;
-	*ptr = 0;
-
-	tempString.length = 0;
-
-	len = fplStringFormat(ptr, tempString.allocated, "%04u-%02u-%02u %02u:%02u:%02u.%03u [%s]: ", utcDateRes.year, utcDateRes.month, utcDateRes.day, utcDateRes.hour, utcDateRes.minute, utcDateRes.second, utcDateRes.millisecond, levelName);
-	tempString.length += len;
-
-	ptr += tempString.length;
-
-	size_t remaining = tempString.allocated - tempString.length - 1;
-
 	va_list argList;
 	va_start(argList, format);
-	len = fplStringFormatArgs(ptr, remaining, format, argList);
+	LogWriteArgs(level, format, argList);
 	va_end(argList);
-
-	tempString.length += len + 1;
-
-	tempString.data[tempString.length - 1] = '\n';
-	tempString.data[tempString.length] = '\0';
-
-	fplFileWriteBlock(&gLog.fileHandle, tempString.data, tempString.length);
-
-	fplFileFlush(&gLog.fileHandle);
-
-	fmemEndTemporary(&temporaryMemory);
 }
 
 #endif // FINAL_LOG_IMPLEMENTATION
